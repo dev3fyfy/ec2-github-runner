7f9a9b37e1ccb0722e0370a898b5d849952a6e2f
Ahmed Afifi <92525726+dev3fyfy@users.noreply.github.com>
2022-12-14 01:25:02 +0200

    Update aws.js

--- src/aws.js
+++ src/aws.js
@@ -1,42 +1,21 @@
 const AWS = require('aws-sdk');
 const core = require('@actions/core');
 const config = require('./config');
-
-// User data scripts are run as the root user
-function buildUserDataScript(githubRegistrationToken, label) {
-  if (config.input.runnerHomeDir) {
-    // If runner home directory is specified, we expect the actions-runner software (and dependencies)
-    // to be pre-installed in the AMI, so we simply cd into that directory and then start the runner
-    return [
-      '#!/bin/bash',
-      `cd "${config.input.runnerHomeDir}"`,
-      'export RUNNER_ALLOW_RUNASROOT=1',
-      `./config.sh --url https://github.com/${config.githubContext.owner}/${config.githubContext.repo} --token ${githubRegistrationToken} --labels ${label}`,
-      './run.sh',
-    ];
-  } else {
-    return [
-      '#!/bin/bash',
-      'mkdir actions-runner && cd actions-runner',
-      'case $(uname -m) in aarch64) ARCH="arm64" ;; amd64|x86_64) ARCH="x64" ;; esac && export RUNNER_ARCH=${ARCH}',
-      'curl -O -L https://github.com/actions/runner/releases/download/v2.286.0/actions-runner-linux-${RUNNER_ARCH}-2.286.0.tar.gz',
-      'tar xzf ./actions-runner-linux-${RUNNER_ARCH}-2.286.0.tar.gz',
-      'export RUNNER_ALLOW_RUNASROOT=1',
-      `./config.sh --url https://github.com/${config.githubContext.owner}/${config.githubContext.repo} --token ${githubRegistrationToken} --labels ${label}`,
-      './run.sh',
-    ];
-  }
-}
-
 async function startEc2Instance(label, githubRegistrationToken) {
   const ec2 = new AWS.EC2();
-
-  const userData = buildUserDataScript(githubRegistrationToken, label);
-  if (config.input.useSpotInstances) {
-    params.InstanceMarketOptions = {
-      MarketType: 'spot'
-    };
-  }
+  // User data scripts are run as the root user.
+  // Docker and git are necessary for GitHub runner and should be pre-installed on the AMI.
+  const userData = [
+    '#!/bin/bash',
+    'mkdir actions-runner && cd actions-runner',
+    'case $(uname -m) in aarch64) ARCH="arm64" ;; amd64|x86_64) ARCH="x64" ;; esac && export RUNNER_ARCH=${ARCH}',
+    'curl -O -L https://github.com/actions/runner/releases/download/v2.278.0/actions-runner-linux-${RUNNER_ARCH}-2.278.0.tar.gz',
+    'tar xzf ./actions-runner-linux-${RUNNER_ARCH}-2.278.0.tar.gz',
+    'export RUNNER_ALLOW_RUNASROOT=1',
+    'export DOTNET_SYSTEM_GLOBALIZATION_INVARIANT=1',
+    `./config.sh --url https://github.com/${config.githubContext.owner}/${config.githubContext.repo} --token ${githubRegistrationToken} --labels ${label}`,
+    './run.sh',
+  ];
   const params = {
     ImageId: config.input.ec2ImageId,
     InstanceType: config.input.ec2InstanceType,
@@ -47,12 +26,27 @@ async function startEc2Instance(label, githubRegistrationToken) {
     SecurityGroupIds: [config.input.securityGroupId],
     IamInstanceProfile: { Name: config.input.iamRoleName },
     TagSpecifications: config.tagSpecifications,
-    BlockDeviceMappings: config.input.blockDeviceMappings,
   };
+
+  if (config.input.useSpotInstances) {
+    params.InstanceMarketOptions = {
+      MarketType: 'spot'
+    };
+  }
 
   try {
     const result = await ec2.runInstances(params).promise();
     const ec2InstanceId = result.Instances[0].InstanceId;
+
+    
+          
+            
+    
+
+          
+    
+    
+  
     core.info(`AWS EC2 instance ${ec2InstanceId} is started`);
     return ec2InstanceId;
   } catch (error) {
@@ -60,14 +54,11 @@ async function startEc2Instance(label, githubRegistrationToken) {
     throw error;
   }
 }
-
 async function terminateEc2Instance() {
   const ec2 = new AWS.EC2();
-
   const params = {
     InstanceIds: [config.input.ec2InstanceId],
   };
-
   try {
     await ec2.terminateInstances(params).promise();
     core.info(`AWS EC2 instance ${config.input.ec2InstanceId} is terminated`);
@@ -77,14 +68,11 @@ async function terminateEc2Instance() {
     throw error;
   }
 }
-
 async function waitForInstanceRunning(ec2InstanceId) {
   const ec2 = new AWS.EC2();
-
   const params = {
     InstanceIds: [ec2InstanceId],
   };
-
   try {
     await ec2.waitFor('instanceRunning', params).promise();
     core.info(`AWS EC2 instance ${ec2InstanceId} is up and running`);
@@ -94,7 +82,6 @@ async function waitForInstanceRunning(ec2InstanceId) {
     throw error;
   }
 }
-
 module.exports = {
   startEc2Instance,
   terminateEc2Instance,
