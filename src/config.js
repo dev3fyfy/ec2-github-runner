c1cebae6dc2a3bbfe687f344a76370e0355136c2
Ahmed Afifi <92525726+dev3fyfy@users.noreply.github.com>
2022-12-14 01:03:20 +0200

    Update config.js

--- src/config.js
+++ src/config.js
@@ -15,6 +15,7 @@ class Config {
       iamRoleName: core.getInput('iam-role-name'),
       runnerHomeDir: core.getInput('runner-home-dir'),
       blockDeviceMappings: JSON.parse(core.getInput('block-device-mappings')),
+      useSpotInstances: core.getBooleanInput('use-spot-instances'),      
     };
 
     const tags = JSON.parse(core.getInput('aws-resource-tags'));
